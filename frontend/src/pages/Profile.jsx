c78fa1f36133a0310053ed94101ee9361da774ac
Darsh Parikh <parikhdarsh121@gmail.com>
2025-06-24 19:49:41 +0530

    Changed the profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,11 @@
 import React, { useContext, useState } from 'react'
 import AppContext from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
     const { setExistingUser } = useContext(AppContext);
+
+    const navigate = useNavigate();
 
     const [image, setImage] = useState('/profile.png');
     const [name, setName] = useState('');
@@ -16,60 +19,66 @@ const Profile = () => {
         }
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        navigate('/');
         setExistingUser(true);
     }
 
     return (
-        <div className='w-full h-screen flex flex-col md:flex-row'>
-            {/* Profile Upload */}
-            <div className='md:w-2/5 w-full md:h-full max-sm:2/3 h-1/2 max-md:flex-row max-sm:flex-col flex-col flex justify-center items-center md:items-center sm:items-end gap-10 md:bg-[#a9a89b] max-md:text-[#a9a89b] max-md:mt-10'>
-                <div className='border-2 md:border-black border-[#a9a89b] aspect-[1/1] md:w-4/5 w-2/5 max-sm:w-60 overflow-hidden p-2 rounded-lg flex justify-center items-center text-center text-sm'>
-                    {image !== '/profile.png' ? <img src={image} alt="Profile Image Preview" className='w-fit h-full object-cover rounded-lg' /> : 'Upload Profile Image'}
+        <div className="flex flex-col md:flex-row w-full h-screen bg-black">
+        {/* Profile Image Section */}
+            <div className="flex flex-col items-center justify-center gap-6 w-full md:w-2/5 lg:w-1/2 h-1/2 md:h-full text-white py-10">
+                <div className="w-40 max-w-[300px] md:w-3/5 aspect-square overflow-hidden rounded-lg border-2 border-white flex items-center justify-center text-sm">
+                    {image !== '/profile.png' ? (
+                        <img src={image} alt="Profile" className="w-full h-full object-cover" />
+                    ) : (
+                        'Upload Profile Image'
+                    )}
                 </div>
-                <label htmlFor="imageUpload" className='bg-transparent border-2 border-dashed text-sm md:border-black border-[#a9a89b] rounded-md max-w-60 py-5 px-7 cursor-pointer flex gap-5 self-center max-sm:mt-0 max-md:mt-15'>
-                    {image !== '/profile.png' ? 'Change Profile Image' : 'Upload Profile Image'}
-                    <img src="/black-upload.png" alt="Upload Icon" className='w-5 h-fit max-md:hidden' />
-                    <img src="/yellow-upload.png" alt="Upload Icon" className='w-5 h-fit md:hidden' />
-                </label>
-                <input 
-                    id='imageUpload'
-                    type="file" 
-                    accept='image/*'
+                <label htmlFor="imageUpload" className="flex items-center gap-3 px-5 py-2 border-2 border-dashed rounded-md cursor-pointer hover:bg-white/10 transition max-md:text-sm">
+                    {image !== '/profile.png' ? 'Change Image' : 'Upload Image'}
+                    <img src="/upload.png" className="w-5" />
+                </label>    
+                <input
+                    type="file"
+                    id="imageUpload"
+                    accept="image/*"
                     onChange={handleImage}
-                    className='hidden'
+                    className="hidden"
                 />
             </div>
 
-            {/* Form Filling */}
-            <div className='md:w-3/5 w-full md:h-full max-sm:h-1/3 h-1/2 max-md:flex-row max-sm:flex-col flex-col flex justify-center items-end'>
-                <form onSubmit={handleSubmit} className='md:bg-[#a9a89b]/30 flex flex-col max-md:items-center max-md:justify-center max-md:w-full max-md:h-full w-4/5 h-3/7 rounded-l-2xl relative px-15'>
-                    <h1 className='absolute max-md:text-2xl max-sm:-top-103 max-md:-top-93 max-md:left-22 text-4xl text-white text-extrabold -top-5 self-start'>User Profile</h1>
-                    <div className='xl:w-[60%] max-sm:w-full w-[90%] max-sm:flex-wrap flex sm:justify-center items-center sm:gap-7 mt-10'>
-                        <label className="text-md text-white w-1/5 sm:px-4">Name</label>
-                        <input 
-                            type="text" 
-                            onChange={(e) => {setName(e.target.value)}}
+            {/* Profile Form Section */}
+            <div className="flex flex-col justify-center items-center md:items-end w-full md:w-3/5 lg:w-1/2 h-1/2 md:h-full text-black md:py-10 pl-10 pb-0 max-md:pr-10 max-sm:p-0">
+                <form onSubmit={handleSubmit} className="w-full h-full max-w-2xl bg-[#a9a89b] py-8 sm:px-8 md:rounded-r-none max-md:rounded-b-none rounded-xl text-sm flex flex-col justify-center items-center">
+                    <h1 className="md:text-4xl text-2xl font-bold text-center mb-5 md:mb-12">User Profile</h1>
+
+                    <div className="mb-6 w-full max-w-[80%]">
+                        <label className="block mb-1 text-black">Name</label>
+                        <input
+                            type="text"
+                            onChange={(e) => setName(e.target.value)}
                             value={name}
                             required
-                            defaultValue={name}
-                            className='w-4/5 max-sm:w-full outline-none text-md py-2 px-4 mt-1 border rounded-lg border-white/30 focus:border-white transition-all duration-200 text-white' 
+                            className="w-full p-2 border border-white rounded-lg focus:outline-none focus:ring focus:border-black transition-all duration-200 text-sm"
                         />
                     </div>
-                    <div className='xl:w-[60%] max-sm:w-full w-[90%] max-sm:flex-wrap flex sm:justify-center items-center sm:gap-8 mt-5'>
-                        <label className="text-md text-white w-1/5 sm:px-4">Bio</label>
-                        <textarea 
-                            onChange={(e) => {setBio(e.target.value)}}
+
+                    <div className="mb-6 w-full max-w-[80%]">
+                        <label className="block mb-1 text-black">Bio</label>
+                        <textarea
+                            onChange={(e) => setBio(e.target.value)}
                             value={bio}
                             rows={4}
                             required
-                            className='w-4/5 max-sm:w-full outline-none text-md py-2 px-4 border rounded-lg border-white/30 focus:border-white transition-all duration-200 text-white resize-none' 
-                        ></textarea>
+                            className="w-full p-2 border border-white rounded-lg resize-none focus:outline-none focus:ring focus:border-black transition-all duration-200 text-sm"
+                        />
                     </div>
+
                     <button
-                        type='submit'
-                        className='xl:w-[60%] max-sm:w-full w-[90%] text-black bg-[#a9a89b] mt-7 py-2 rounded-lg sm:ml-4 cursor-pointer hover:scale-[1.02] transition-all duration-300'
+                        type="submit"
+                        className="w-[80%] py-2 bg-[#c7c5b6] text-black rounded-lg hover:scale-[1.02] transition-all duration-500 cursor-pointer mt-6 font-semibold"
                     >
                         Save
                     </button>
